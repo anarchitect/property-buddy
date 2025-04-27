56004c239e452df12673e57d7b7458855124b3c6
Jia Ji Chen <aulotus@gmail.com>
2025-04-27 21:33:11 +1000

    remove chat without file

--- main.py
+++ main.py
@@ -97,45 +97,6 @@ async def show_profile(request: Request):
         {"request": request, "resident": resident}
     )
 
-@app.post("/chat")
-async def chat(message: HistoryMessage):
-
-    system_message = {"role": "system", "content": "You are a helpful, concise, and professional chatbot assisting users."}
-    chat_messages = [{"role": m.role, "content": m.content} for m in message.history]
-    full_conversation = [system_message] + chat_messages
-    final_message =""
-
-    response = client.chat.completions.create(
-        messages=full_conversation,
-        functions=functions,
-        function_call="auto",
-        max_tokens=4096,
-        temperature=1,
-        top_p=1.0,
-        model=os.getenv("AZURE_DEPLOYMENT_ID"))
-    
-    first_response = response.choices[0].message
-
-    if first_response.function_call:
-        function_name = first_response.function_call.name
-        arguments = json.loads(first_response.function_call.arguments)
-
-        # Step 6. Call the utility function to process the function call
-        function_response = process_function_call(function_name, arguments)
-
-        second_response = handle_function_call_response(client, first_response, function_response, full_conversation, function_name)
-
-        # Step 8. Print final model response!
-        final_message = second_response.choices[0].message.content
-        print("🤖 AI says:", final_message)
-
-    else:
-        # No function call, just reply
-        final_message = first_response.content
-        print("🤖 AI says:", final_message)
-
-    return {"reply": final_message}
-
 @app.post("/chat_with_upload/")
 async def chat_with_upload(
     message: str = Form(None),
